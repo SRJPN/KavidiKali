9a0cdfb736468e28102dcd5a4cc7a4ec7295d2ae
Sooraj Parameswaran <soorajp@thoughtworks.com>
2017-12-20 22:05:06 +0530

    Sooraj Adding heroku config for the app

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var requestHandler = require('./routing.js');
 var IP_ADDRESS = process.env.OPENSHIFT_NODEJS_IP;
-var PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var PORT = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
 var games = {};
 var controller = requestHandler(games);
 
